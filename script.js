74a1622e93f8f7c3e34ad8cbe03e356e98996101
amoruch <roma.usha@yandex.ru>
2024-12-07 15:04:43 +0300

    basic begining
    html structure
    some css style
    + javascript code for templates

---
+++ script.js
@@ -0,0 +1,13 @@
+let pokemon_cards = document.getElementById("pokemon_cards");
+let card_template = document.getElementById("card");
+let tag_template = document.getElementById("tag");
+
+for (let i = 0; i < 10; i++) {
+    pokemon_cards.appendChild(card_template.content.cloneNode(true));
+    let elem = document.getElementById("0");
+    elem.id = i + 1;
+
+    let tags = elem.getElementsByClassName("tags")[0]
+    tags.appendChild(tag_template.content.cloneNode(true));
+    tags.appendChild(tag_template.content.cloneNode(true));
+}
