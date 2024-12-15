e1c4ef5d3b25b82b97c0ed6812eef4e8e162ca53
amoruch <roma.usha@yandex.ru>
2024-12-15 21:02:09 +0300

    fucking fetch

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ let pokemon_cards = document.getElementById("pokemon_cards");
 let card_template = document.getElementById("card");
 let tag_template = document.getElementById("tag");
 
-for (let i = 0; i < 12; i++) {
-    pokemon_cards.appendChild(card_template.content.cloneNode(true));
-    let elem = document.getElementById("0");
-    elem.id = i + 1;
+let api = "https://pokeapi.co/api/v2/pokemon";
 
-    let tags = elem.getElementsByClassName("tags")[0]
-    tags.appendChild(tag_template.content.cloneNode(true));
-    tags.appendChild(tag_template.content.cloneNode(true));
+let image_url = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/";
+
+let all_pokemons;
+
+function f(x) {
+    return "0".repeat(3 - x.toString().length) + x;
 }
+
+async function aboba() {
+    let x = await fetch(api);
+    let y = await x.json();
+    all_pokemons = y.results;
+
+    for (let i = 0; i < 12; i++) {
+        pokemon_cards.appendChild(card_template.content.cloneNode(true));
+
+        let elem = document.getElementById("0");
+        let aboba = all_pokemons[i].name;
+        elem.getElementsByClassName("name")[0].innerText = aboba;
+        elem.getElementsByTagName("img")[0].src = image_url + f(i + 1) + ".png";
+        elem.id = i + 1;
+    
+        let tags = elem.getElementsByClassName("tags")[0];
+        tags.appendChild(tag_template.content.cloneNode(true));
+        tags.appendChild(tag_template.content.cloneNode(true));
+    }
+}
+
+aboba();
