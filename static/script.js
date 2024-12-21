01b4468000c8ac3e24800783e33baa74df03f0ab
amoruch <roma.usha@yandex.ru>
2024-12-21 17:42:06 +0300

    js code for cool cards

--- static/script.js
+++ static/script.js
@@ -7,6 +7,28 @@ let image_url = "https://www.pokemon.com/static-assets/content-assets/cms2/img/p
 
 let page = 1;
 
+let tag_colors = {
+    "grass": ["green", "#00AA00"],
+    "poison": ["#40FD14", "#11DD11"],
+    "fire": ["red", "#FF5555"],
+    "flying": ["lightblue", "#BBBBBB"],
+    "water": ["blue", "#0099FF"],
+    "bug": ["orange", "#55DD55"],
+    "normal": ["gray", "#AAAAAA"],
+    "electric": ["#00F0FF", "#00C0CC"],
+    "ground": ["brown", "#967B00"],
+    "fairy": ["pink", "lightpink"],
+    // here goes random colors
+    "fighting": ["#F54021", "#FF7514"],
+    "psychic": ["#00BB2D", "#E6D690"],
+    "rock": ["#025669", "#3B3C36"],
+    "steel": ["#6C7059", "#35682D"],
+    "ice": ["#7FB5B5", "#82898F"],
+    "ghost": ["#6C3B2A", "#6C7156"],
+    "dragon": ["#999950", "#D0D0D0"],
+    "dark": ["#EA899A", "#B32821"]
+};
+
 function get_page() {
     let url_string = window.location.href; 
     let url = new URL(url_string);
@@ -18,30 +40,48 @@ function f(x) {
     return "0".repeat(Math.max(0, 3 - x.toString().length)) + x;
 }
 
-function load_pokemons() {
+async function get_pokemon_types(name) {
+    let x = await fetch("https://pokeapi.co/api/v2/pokemon/" + name);
+    let y = await x.json();
+    let pokemon_types = y.types;
+    let types = [];
+    for (let i = 0; i < pokemon_types.length; i++) {
+        types.push(pokemon_types[i].type.name);
+    }
+    return types;
+}
+
+async function load_pokemons() {
     for (let i = 0; i < 20; i++) {
         pokemon_cards.appendChild(card_template.content.cloneNode(true));
 
         let elem = document.getElementById("0");
         elem.id = i + 1;
-    
-        let tags = elem.getElementsByClassName("tags")[0];
-        tags.appendChild(tag_template.content.cloneNode(true));
-        tags.appendChild(tag_template.content.cloneNode(true));
     }
 }
 
-async function load_images() {
+async function load_content() {
     let x = await fetch(api + ((page - 1) * 20));
     let y = await x.json();
     let all_pokemons = y.results;
-    api = y.next;
     for (let i = 0; i < all_pokemons.length; i++) {
         let elem = document.getElementById(i + 1);
-        let aboba = all_pokemons[i].name;
+        let name = all_pokemons[i].name;
 
-        elem.getElementsByClassName("name")[0].innerText = aboba;
+        elem.getElementsByClassName("name")[0].innerText = name;
+        elem.getElementsByClassName("id")[0].innerText = "#" + f((page - 1) * 20 + i + 1);
         elem.getElementsByTagName("img")[0].src = image_url + f((page - 1) * 20 + i + 1) + ".png";
+
+        let types = await get_pokemon_types(name);
+        let tags = elem.getElementsByClassName("tags")[0];
+        for (let i = 0; i < types.length; i++) {
+            tags.appendChild(tag_template.content.cloneNode(true));
+            let type_button = tags.getElementsByTagName("button")[i]
+            let p = type_button.getElementsByTagName("p")[0];
+            p.innerText = types[i];
+            type_button.style.backgroundColor = tag_colors[types[i]][0];
+        }
+        elem.style.backgroundColor = tag_colors[types[0]][1];
     }
 }
 
@@ -67,4 +107,4 @@ function next_page() {
 
 page = get_page()
 load_pokemons();
-load_images();
+load_content();
